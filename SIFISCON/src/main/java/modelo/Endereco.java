09814e35af0be93f773fc4853d308a839ae9e199
Luis <Luis@Luis-Gomes>
2016-09-25 16:52:32 -0300

    Adicionado o Fornecedor e o endereço do fornecedor.

---
+++ SIFISCON/src/main/java/modelo/Endereco.java
@@ -0,0 +1,92 @@
+package modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Endereco {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(nullable = false)
+	private String logradouro;
+	
+	@Column(nullable = false)
+	private String numero;
+	
+	private String complemento;
+	
+	@Column(nullable = false)
+	private String bairro;
+	
+	@Column(nullable = false)
+	private String municipio;
+	
+	@Column(nullable = false)
+	private String cep;
+	
+	@Column(nullable = false)
+	private String UF;
+	
+	@OneToOne
+	private Fornecedor fornecedor;	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getUF() {
+		return UF;
+	}
+	public void setUF(String uF) {
+		UF = uF;
+	}
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+}
--- SIFISCON/src/main/java/modelo/Fornecedor.java
+++ SIFISCON/src/main/java/modelo/Fornecedor.java
@@ -1,8 +1,30 @@
 package modelo;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 public class Fornecedor {
-	private int id;
+	@Id
+	@GeneratedValue
+	private int id;	
+	
+	@Column(nullable = false, unique = true)
 	private String CNPJ;
+	
+	@Column(nullable = false)
+	private String razaoSocial;	
+	
+	private String incricaoMunicipal;
+	
+	@Column(nullable = false)
+	private BigDecimal receitaBruta;
+	
+	@OneToOne	
+	private Endereco endereco;	
 	
 	public int getId() {
 		return id;
@@ -16,4 +38,28 @@ public class Fornecedor {
 	public void setCNPJ(String cNPJ) {
 		CNPJ = cNPJ;
 	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getIncricaoMunicipal() {
+		return incricaoMunicipal;
+	}
+	public void setIncricaoMunicipal(String incricaoMunicipal) {
+		this.incricaoMunicipal = incricaoMunicipal;
+	}
+	public BigDecimal getReceitaBruta() {
+		return receitaBruta;
+	}
+	public void setReceitaBruta(BigDecimal receitaBruta) {
+		this.receitaBruta = receitaBruta;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}	
 }
